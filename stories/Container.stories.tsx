a85b40983af40a2ade1a5f1c7a48973c62026d01
Victor Corassa <victorcorassa@gmail.com>
2022-06-16 13:58:49 -0300

    fix: story styled view

--- stories/Container.stories.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import { Container } from "./components/Container";
-
-storiesOf("Container", module).add("Default", () => <Container />);
---
+++ stories/StyledView.stories.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { StyledView } from "./components/StyledView";
+
+storiesOf("Container", module).add("Default", () => <StyledView />);
